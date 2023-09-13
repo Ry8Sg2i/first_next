36e2c3ccd884f3497f16593c8a55379509c9055e
Ryu <azatose8684@gmail.com>
2023-09-13 13:37:43 +0900

    apiにまとめた

--- src/api.ts
+++ src/api.ts
@@ -13,5 +13,22 @@ export async function doneTodo(id: number, isCompleted: boolean) {
     },
   });
   revalidatePath('/posts');
-}
-  
+};
+
+export async function addTodo(data:FormData) {
+  'use server';
+    const name = data.get('name') as string;
+    await prisma.todo.create({data: {name}});
+    revalidatePath("/")
+  };
+
+export async function deleteTodo(data:FormData) {
+  'use server';
+    const id = data.get('id') as string;
+    await prisma.todo.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    revalidatePath('/')
+  };
